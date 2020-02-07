3a1ea0300420e42817d11c19241c29541ba74145
geeveebee <gvonbush@me.com>
2020-02-06 20:58:41 -0500

    made sound functions for victory, defeat and attack

--- public/js/battlefield.js
+++ public/js/battlefield.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+     // creating howler for the music
+  const battletheme = new Howl({
+    src: ["../soundfiles/Music/battle.mp3"],
+    loop: true,
+    autoplay: true,
+  });
+
+  // play the music
+  battletheme.fadeIn(10);
+
+
+
+
 
 // set up a function to display the battlefield, be able to call that whenever we need to
 // on-click listeners
