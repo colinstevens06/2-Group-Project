486d7953e475557568fc4157be0d53ff37132737
Colin Stevens <colinstevens06@gmail.com>
2020-02-06 20:15:29 -0500

    added first sounds

---
+++ public/js/battlefield.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+
+// set up a function to display the battlefield, be able to call that whenever we need to
+// on-click listeners
+
+// all the buttons (use one class), on click, battlefield.attack(clicked-button.text())
+
+
+});
