8ed26e764bb01f43330e2555ba0897d257be8bef
Kina Rain <tina@lawtk.net>
2020-02-07 20:22:06 -0500

    updated pages

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
 const path = require("path");
+const app = express();
 
-var app = express();
-
-var PORT = process.env.PORT || 8080;
+//set the port for the app
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +11,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/public/html"));
+app.engine("html", require("ejs").renderFile);
 
 var routes = require("./src/controllers/pokemon_controller");
 
--- src/controllers/pokemon_controller.js
+++ src/controllers/pokemon_controller.js
@@ -1,8 +1,7 @@
 var express = require("express");
-
 var router = express.Router();
+var poke_user = require("../models/poke_user");
 
-//var pokemon = require("../models/pokemon.js");
 
 router.post("/api/new", function (req, res) {
   console.log("Pokemon Data:");
@@ -12,11 +11,32 @@ router.post("/api/new", function (req, res) {
 });
 
 router.get("/", function (req, res) {
-  res.render("index");
+  res.render("index.html");
 });
 
+router.get("/leaderboard", function(req, res) {
+  res.render("leaderboard.html");
+});
+
+router.get("/battlefield", function(req, res) {
+  res.render("battlefield.html");
+})
+
 router.get("/profile", function (req, res) {
-  res.render("partials/profile", objHbrsData);
+
+  poke_user.findOne({
+    attributes: ["user_ID", "user_charName", "user_Fname", "user_Lname", "user_email"]})
+    .then(profileData => {
+      let userData = {
+        user_ID: profileData.get("user_ID"),
+        user_charName: profileData.get("user_charName"),
+        user_Fname: profileData.get("user_Fname"),
+        user_Lname: profileData.get("user_Lname"),
+        user_email: profileData.get("user_email")
+      };
+      res.render("profile.html", userData);
+    })
+    .catch(err => console.log("Error: " + err));
 });
 
 module.exports = router;
