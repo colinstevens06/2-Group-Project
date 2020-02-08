51bf327a1e5698717c296434165839777e1ba259
jonoaugustine <jonoaugustine@jhu.edu>
2020-02-08 03:40:39 -0500

    moved matching to game.js

--- public/js/ws/game.js
+++ public/js/ws/game.js
@@ -63,3 +63,27 @@ class Battlefield {
     });
   }
 }
+
+let client = new Colyseus.Client("ws://localhost:7000"); // TODO Update to heroku URI
+
+/**
+ * Finds or creates a new lobby.
+ *
+ * @returns {Battlefield} the Colyseus room that was joined or made
+ */
+const findGame = async () => {
+  let room;
+
+  try {
+    room = await client.joinOrCreate("battle", {
+      username: "user-" + Math.random(),
+      lobbyName: "BF-" + Math.random(),
+      authToken: null // TODO Auth tokens
+    });
+  } catch (error) {
+    console.log("FAILED ", error);
+    return null;
+  }
+
+  return new Battlefield(room);
+};
